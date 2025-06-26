dfafb5737db11519f979ba28a7971f02271d9f0b
Victor Onyekwere <cydollarsign1@gmail.com>
2025-06-26 07:35:38 -0400

    Firebase Update

--- src/features/auth/firebase.js
+++ src/features/auth/firebase.js
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_K || "",
+  authDomain: import.meta.env.VITE_FIREBASE_ATH_DOMAIN || "",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
