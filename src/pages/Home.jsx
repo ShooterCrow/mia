0ef2087a1b2025f4eb99388527b9a019bbc1636c
Victor Onyekwere <cydollarsign1@gmail.com>
2025-07-06 10:43:07 -0400

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -96,7 +96,7 @@ function Home() {
             {/* Promotions Section */}
             <section className="lg:py-10 sm:pb-12 bg-white dark:bg-gray-800 transition-colors duration-200">
                 <div className="max-w-7xl mx-auto px-3 sm:px-4 md:px-6 lg:px-8">
-                    <h2 className="text-2xl sm:text-3xl font-bold text-gray-900 dark:text-white mb-8 sm:mb-12 sm:text-left">Promotions Ad</h2>
+                    <h2 className="text-2xl sm:text-3xl font-bold text-gray-900 dark:text-white my-4 sm:mb-12 sm:text-left">Promotions Ad</h2>
                     <ProductCard1 products={products} />
 
                 </div>
