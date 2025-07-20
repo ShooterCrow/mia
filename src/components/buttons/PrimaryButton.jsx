13af0a224a72acc5c36919a47601d69b09e7f7a8
Victor Onyekwere <cydollarsign1@gmail.com>
2025-07-20 16:23:59 -0400

    Updates on Profile Section

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PrimaryButton = ({ text, link }) => {
     return (
         <div>
-            <button className="bg-red-600 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800 text-white px-6 sm:px-8 sm:py-2 rounded-lg font-semibold text-sm sm:text-base transition-colors duration-200 w-fit sm:w-fit">
+            <button className="bg-red-600 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800 text-white px-6 sm:px-8 py-2 rounded-lg font-semibold text-sm sm:text-base transition-colors duration-200 w-fit sm:w-fit">
                 <Link to={link}>
                     {text}
                 </Link>
