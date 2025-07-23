ceced9cc8ea1483a541f4b5c3797684f8d02f42d
Victor Onyekwere <cydollarsign1@gmail.com>
2025-07-23 09:43:24 -0400

    Vic update

--- src/features/user/ProductCategories.jsx
+++ src/features/user/ProductCategories.jsx
@@ -38,7 +38,7 @@ const ProductCategories = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <div className="mx-auto">
       {/* Selected Categories Header */}
       <div className="mb-6">
         <h2 className="text-lg font-medium text-gray-700 dark:text-gray-300 mb-3">
